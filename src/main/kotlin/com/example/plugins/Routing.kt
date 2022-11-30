5ceb6b9724fb24a1a06bfce681778f75acf655a4
Italo <you@example.com>
2022-11-30 17:41:04 -0300

    Koin working

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.repository.MessageRepository
 import com.example.service.MessageService
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
@@ -10,13 +11,8 @@ fun Application.configureRouting() {
 
     val messageService by inject<MessageService>()
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
         get("/messages") {
             call.respond(messageService.getMessages())
         }
-    }
-    routing {
     }
 }
